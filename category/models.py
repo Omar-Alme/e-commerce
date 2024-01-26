e5d2c85f7bccacc8142ae0502b236de02d35f466
Omar-Alme <omar.almehdar17@gmail.com>
2024-01-26 01:17:14 +0100

    add category app and models

---
+++ category/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='photo/categories', blank=True)
+
+
+    def __str__(self):
+        return self.name
