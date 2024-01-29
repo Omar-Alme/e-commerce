e7f93aeabce3bd7c64eaba0e782f33467aaa49bd
Omar-Alme <omar.almehdar17@gmail.com>
2024-01-29 14:57:27 +0100

    add single product view

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.products, name='products'),
+    path('<slug:category_slug>/', views.products, name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
+
 
 # Create your views here.
 
-def products(request):
-    """ A view to return the products page """
+def products(request, category_slug=None):
+    """ A view to return the products page and filter by category """
+    categories=None
+    products=None
 
-    products = Product.objects.all().filter(is_available=True)
-    product_count = products.count()
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:   
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
 
     context = {
         'products': products,
@@ -15,3 +23,21 @@ def products(request):
     }
 
     return render(request, "products/products.html", context)
+
+
+
+def product_detail(request, category_slug, product_slug):
+    """ A view to return the product detail page for a specific product"""
+
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    
+    context = {
+        'single_product': single_product,
+    }
+    
+
+
+    return render(request, "products/product_detail.html", context)
