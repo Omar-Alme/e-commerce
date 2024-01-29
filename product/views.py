3d52144524aa6cddc5057df491d8d5365771e4d9
Omar-Alme <omar.almehdar17@gmail.com>
2024-01-29 10:21:00 +0100

    add product count feature

--- product/views.py
+++ product/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def products(request):
-    return render(request, "products/products.html")
+    """ A view to return the products page """
+
+    products = Product.objects.all().filter(is_available=True)
+    product_count = products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+
+    return render(request, "products/products.html", context)
