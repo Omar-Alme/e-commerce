0f6d85475a30d1d52ba19f2756a9cb1b970ac1b3
Omar-Alme <omar.almehdar17@gmail.com>
2024-01-31 20:20:41 +0100

    add grand total and tax calculations

--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from product.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
-def cart(request):
+def cart(request, total=0, quantity=0, cart_items=None):
     """ A view to return the cart page """
-    return render(request, "products/cart.html")
+
+    try:
+        cart = Cart.objects.get(cart_id=cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+
+        tax = (2 * total) / 100
+        grand_total = total + tax
+
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
+    }
+
+    return render(request, "products/cart.html", context)
 
 
 
@@ -26,23 +49,22 @@ def add_to_cart(request, product_id):
         cart = Cart.objects.get(cart_id=cart_id(request))
     except Cart.DoesNotExist:
         cart = Cart.objects.create(
-            cart_id = cart_id(request)
+            cart_id=cart_id(request)
         )
         cart.save()
 
-        try:
-            cart_item = CartItem.objects.get(product=product, cart=cart)
-            if cart_item.quantity < cart_item.product.stock:
-                cart_item.quantity += 1
-            cart_item.save()
-        except CartItem.DoesNotExist:
-            cart_item = CartItem.objects.create(
-                product = product,
-                quantity = 1,
-                cart = cart
-            )
-            cart_item.save()
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart,
+        )
+        cart_item.save()
 
-        return redirect('cart')
+    return redirect('cart')
 
 
