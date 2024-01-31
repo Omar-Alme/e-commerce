1bb090d95a9cd2e65b197dbaed449097eb9c9059
Omar-Alme <omar.almehdar17@gmail.com>
2024-01-31 23:52:10 +0100

    add site pagination functionality

--- product/views.py
+++ product/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from cart.models import CartItem, Cart
+from cart.views import cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 # Create your views here.
@@ -12,14 +15,21 @@ def products(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 2)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:   
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 2)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
+
     }
 
     return render(request, "products/products.html", context)
@@ -31,6 +41,7 @@ def product_detail(request, category_slug, product_slug):
 
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+
     except Exception as e:
         raise e
     
@@ -38,6 +49,5 @@ def product_detail(request, category_slug, product_slug):
         'single_product': single_product,
     }
     
-
 
     return render(request, "products/product_detail.html", context)
