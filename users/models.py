040c07feb8ed1cf1bed69d13f653dbffaca2024a
Omar-Alme <omar.almehdar17@gmail.com>
2024-02-06 23:10:41 +0100

    fix user model is active

--- users/models.py
+++ users/models.py
@@ -49,14 +49,14 @@ class User(AbstractBaseUser):
     last_name = models.CharField(max_length=200)
     username = models.CharField(max_length=200, unique=True)
     email = models.EmailField(max_length=200, unique=True)
-    phone_number = models.CharField(max_length=50, unique=True)
+    phone_number = models.CharField(max_length=50)
 
     # required
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
