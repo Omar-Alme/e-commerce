0045e3d810ef941d381a704ae5e8ad4df6e85e12
Omar-Alme <omar.almehdar17@gmail.com>
2024-02-07 23:18:40 +0100

    add email configurations

--- superkart/settings.py
+++ superkart/settings.py
@@ -94,7 +94,14 @@ AUTHEMTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'info.dripdrop.se@gmail.com'
+EMAIL_HOST_PASSWORD = 'jloz xwcx wxlc ecja'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
