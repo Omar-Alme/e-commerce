7eb432aa36a62cb8dff411044f5507e77c29dd05
Omar-Alme <omar.almehdar17@gmail.com>
2024-02-11 13:17:09 +0100

    add orders app and models

---
+++ orders/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+from users.models import User
+from product.models import Product, Product_options
+
+# Create your models here.
+
+class Payment(models.Model):
+    """A model to store payment details"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment_id = models.CharField(max_length=100)
+    payment_method = models.CharField(max_length=100)
+    amount_paid = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.payment_id
+    
+
+class Order(models.Model):
+    """A model to store order details"""
+
+
+    STATUS = (
+        ('New', 'New'),
+        ('Accepted', 'Accepted'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, 
+                                null=True)
+    order_number = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=250)
+    address1 = models.CharField(max_length=250)
+    address2 = models.CharField(max_length=250, blank=True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=20)
+    phone = models.CharField(max_length=15)
+    order_note = models.TextField(max_length=250, blank=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2)
+    tax = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=100, choices=STATUS, default='New')
+    ip = models.CharField(max_length=20, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_ordered = models.BooleanField(default=False)
+
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.user.first_name
+    
+
+class OrderItem(models.Model):
+    """A model to store order items"""
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, 
+                                null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_options = models.ForeignKey(Product_options, blank=True, 
+                                        on_delete=models.CASCADE)
+    colour = models.CharField(max_length=100, blank=True)
+    size = models.CharField(max_length=100, blank=True)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    ordered = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.product.product_name
