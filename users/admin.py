5e3df0478a6e3007555d53ae6c95d03bfdab9310
Omar-Alme <omar.almehdar17@gmail.com>
2024-03-26 06:54:20 +0100

    add media

--- users/admin.py
+++ users/admin.py
@@ -29,10 +29,13 @@ class UserAdmin(UserAdmin):
 
 class UserProfileAdmin(admin.ModelAdmin):
     def thumbnail(self, object):
-        return format_html(
-            '<img src="{}" width="40" style="border-radius: 50%;">'.format(
-                object.profile_picture.url)
-                )
+        if object.profile_picture:
+            return format_html(
+                '<img src="{}" width="40" style="border-radius: 50%;">'.format(
+                    object.profile_picture.url)
+                    )
+        else:
+            return 'No Image'
     thumbnail.short_description = 'Profile Picture'
     list_display = ('thumbnail', 'user', 'city', 'state', 'country')
 
